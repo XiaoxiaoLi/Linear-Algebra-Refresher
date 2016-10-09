c6d8ba53e6205bd5a0e8c6fe36150f129ebda2c8
XiaoxiaoLi <xiaomao@Damaos-MacBook-Pro.local>
2016-10-09 14:23:03 -0400

    Add Answer to quiz1-1 in python notebook

--- vector.py
+++ vector.py
@@ -19,3 +19,13 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def add(self, v):
+        try:
+            if self.dimension != v.dimension:
+                raise ValueError
+            return Vector(sum(x) for x in zip(self.coordinates, v.coordinates))
+
+        except ValueError:
+            raise ValueError('The vector dimensions must be the same: dimension: '+ str(self.dimension)+" given vector dimension: "+str(v.dimension))
+
